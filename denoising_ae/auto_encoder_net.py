d73259d355d2539ba05527eb64f32349a96fe28d
Brosig <jo77pihe@fauad.fau.de>
2021-11-25 18:48:00 +0100

    Initial autoencoder

--- denoising_ae/auto_encoder_net.py
+++ denoising_ae/auto_encoder_net.py
@@ -21,31 +21,32 @@ class Autoencoder(nn.Module):
         # decoder layers
         # self.dec1 = nn.ConvTranspose3d(8, 8, kernel_size=2, stride=(2, 2, 2)).to(self.device)
         # self.dec2 = nn.ConvTranspose3d(16, 16, kernel_size=(2, 2, 2), stride=(2, 2, 2)).to(self.device)
-        self.dec3 = nn.ConvTranspose3d(32, 32, kernel_size=(2, 2, 2), stride=(2, 2, 2)).to(self.device)
+        self.dec3 = nn.ConvTranspose3d(16, 32, kernel_size=(2, 2, 2), stride=(2, 2, 2)).to(self.device)
         self.dec4 = nn.ConvTranspose3d(32, 64, kernel_size=(2, 2, 2), stride=(2, 2, 2)).to(self.device)
-        # self.dec5 = nn.ConvTranspose3d(64, 128, kernel_size=(3, 3, 3), stride=2).to(self.device)
+        # self.dec5 = nn.ConvTranspose3d(64, 128, kernel_size=2, stride=2).to(self.device)
         self.out = nn.Conv3d(64, 1, kernel_size=(3, 3, 3), padding=1).to(self.device)
 
     def forward(self, x):
-        # encode
-        # x = F.relu(self.enc1(x))
-        # x = self.pool(x)
         if not torch.is_tensor(x):
             x = x[:,:,:,:,0]
             x = x[np.newaxis,:,:,:,:]
             x = torch.Tensor(x).to(self.device)
+
+        # encode
+        # x = F.relu(self.enc1(x))
+        # x = self.pool(x)
         x = F.relu(self.enc2(x))
         x = self.pool(x)
         x = F.relu(self.enc3(x))
-        # x = self.pool(x)
-        # x = F.relu(self.enc4(x))
+        x = self.pool(x)
+        x = F.relu(self.enc4(x))
         # x = self.pool(x)
         # x = F.relu(self.enc5(x))
         x = self.pool(x)  # the latent space representation
 
         # decode
         # x = F.relu(self.dec1(x))
-        # x = F.relu(self.dec2(x))
+        x = F.relu(self.dec2(x))
         x = F.relu(self.dec3(x))
         x = F.relu(self.dec4(x))
         # x = F.relu(self.dec5(x))

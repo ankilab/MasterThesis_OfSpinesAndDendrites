e566b5225f978175f27524d55f2fb4b6096fff98
Brosig <jo77pihe@fauad.fau.de>
2021-11-26 15:24:29 +0100

    Added structured experiment for Levels of Mu-Net and CARE

---
+++ deconv_nets_experiments.py
@@ -0,0 +1,98 @@
+import yaml
+from data_augmentation import DataAugmenter, DataProvider
+from deconv import REGISTRY
+
+# Load configuration
+with open('.\\config.yaml', "r") as stream:
+    args = yaml.safe_load(stream)
+
+######################### Mu-Net #######################################################################################
+
+######################### 3 Levels #####################################################################################
+# Train model
+args['n_levels'] = 3
+args['result_path'] = './Mu_Net_res_3_levels'
+data_provider = DataProvider((args['z_shape'], args['xy_shape']), args['data_path'], args['source_folder'],
+                             args['target_folder'],
+                             data_file='C:/Users/jo77pihe/Documents/MasterThesis_OfSpinesAndDendrites/data32_128.h5')
+deconvolver = REGISTRY['mu-net'](args)
+model_dir, _ = deconvolver.train(data_provider, args['epochs'], args['batch_size'])
+
+# predict image
+test_data = 'D:/jo77pihe/Registered/Raw_32/Test'
+deconvolver.predict(test_data, model_dir)
+
+######################### 2 Levels #####################################################################################
+# Train model
+args['n_levels'] = 2
+args['result_path'] = './Mu_Net_res_2_levels'
+data_provider = DataProvider((args['z_shape'], args['xy_shape']), args['data_path'], args['source_folder'],
+                             args['target_folder'],
+                             data_file='C:/Users/jo77pihe/Documents/MasterThesis_OfSpinesAndDendrites/data32_128.h5')
+deconvolver = REGISTRY['mu-net'](args)
+model_dir, _ = deconvolver.train(data_provider, args['epochs'], args['batch_size'])
+
+# predict image
+test_data = 'D:/jo77pihe/Registered/Raw_32/Test'
+deconvolver.predict(test_data, model_dir)
+
+######################### 1 Levels #####################################################################################
+# Train model
+args['n_levels'] = 1
+args['result_path'] = './Mu_Net_res_1_levels'
+data_provider = DataProvider((args['z_shape'], args['xy_shape']), args['data_path'], args['source_folder'],
+                             args['target_folder'],
+                             data_file='C:/Users/jo77pihe/Documents/MasterThesis_OfSpinesAndDendrites/data32_128.h5')
+deconvolver = REGISTRY['mu-net'](args)
+model_dir, _ = deconvolver.train(data_provider, args['epochs'], args['batch_size'])
+
+# predict image
+test_data = 'D:/jo77pihe/Registered/Raw_32/Test'
+deconvolver.predict(test_data, model_dir)
+
+######################### 0 Levels #####################################################################################
+# Train model
+args['n_levels'] = 0
+args['result_path'] = './Mu_Net_res_0_levels'
+data_provider = DataProvider((args['z_shape'], args['xy_shape']), args['data_path'], args['source_folder'],
+                             args['target_folder'],
+                             data_file='C:/Users/jo77pihe/Documents/MasterThesis_OfSpinesAndDendrites/data32_128.h5')
+deconvolver = REGISTRY['mu-net'](args)
+model_dir, _ = deconvolver.train(data_provider, args['epochs'], args['batch_size'])
+
+# predict image
+test_data = 'D:/jo77pihe/Registered/Raw_32/Test'
+deconvolver.predict(test_data, model_dir)
+
+######################### CARE #########################################################################################
+
+########################################################################################################################
+
+args['result_path'] = './CARE_res'
+args['learning_rate'] = 0.0004
+
+# Generate training data
+data_augmenter = DataAugmenter(args)
+_, data_dir = data_augmenter.augment(args['data_path'], args['source_folder'], args['target_folder'], care=True,
+                                     data_dir=args['data_augmented_path'])
+# # Train CARE model
+deconvolver = REGISTRY['csbdeep'](args)
+
+model_dir, mdl_path = deconvolver.train(data_dir, args['validation_split'], args['epochs'], args['batch_size'])
+
+# predict image
+test_data='D:/jo77pihe/Registered/Raw_32/Test'
+deconvolver.predict(test_data, model_dir= deconvolver.res_path, name=model_dir, save_res=True)
+
+###############################################Auto-Encoder#############################################################
+
+########################################################################################################################
+
+# denoiser = REGISTRY['autoencoder'](args)
+# model_dir, train_history = denoiser.train(args['epochs'], args['batch_size'])
+# X=io.imread('D:/jo77pihe/Registered/Raw_32/Test/Alessandro_427_ArcCreERT2_Thy1GFP_Ai9_TRAP_2019-08-31_A4.tif')
+# img = denoiser.predict_img(X,(32,64,64), 8192, 1)
+# tifffile.imsave('denoiser.tif', img)
+#
+# with open('trainj_hist_den.pkl', 'wb') as outfile:
+#     pickle.dump(train_history, outfile, pickle.HIGHEST_PROTOCOL)
